cdfe25220c47434b5f1f20291546022160d18477
Maxi Bustos <maxiabustos@gmail.com>
2022-03-14 21:04:59 -0300

    New Deployment

---
+++ precache-manifest.93e25adadae92b4edc7871d311bfbe93.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280b1ad754d41bd91df5",
+    "url": "/JoelDarioPerez.github.io/css/app.231d77d0.css"
+  },
+  {
+    "revision": "a9c53ee77e1da5932aa3",
+    "url": "/JoelDarioPerez.github.io/css/chunk-vendors.851df2f9.css"
+  },
+  {
+    "revision": "3b6145d1a103d86fe60e6ea23a18ab14",
+    "url": "/JoelDarioPerez.github.io/img/aero.3b6145d1.png"
+  },
+  {
+    "revision": "8a9de912111fbb561655e12bcc385776",
+    "url": "/JoelDarioPerez.github.io/img/agroIndustria.8a9de912.png"
+  },
+  {
+    "revision": "a4186a7f3bce49b5e871f0a27e2ae024",
+    "url": "/JoelDarioPerez.github.io/img/construccion.a4186a7f.png"
+  },
+  {
+    "revision": "69a32f8a949c219c98c8cce2cd489f69",
+    "url": "/JoelDarioPerez.github.io/img/contactanos.69a32f8a.png"
+  },
+  {
+    "revision": "f41468308313d3d884ec5a864185764d",
+    "url": "/JoelDarioPerez.github.io/img/el-deposito.f4146830.png"
+  },
+  {
+    "revision": "3aed562cacaa1f59fe8b1e080475da74",
+    "url": "/JoelDarioPerez.github.io/img/globalTrackerLogo.3aed562c.png"
+  },
+  {
+    "revision": "4b981b73a7b7abf17eca539dbe2656ba",
+    "url": "/JoelDarioPerez.github.io/img/hidrobombas.4b981b73.png"
+  },
+  {
+    "revision": "80b2b6745753067b59a1b9c83ee7c138",
+    "url": "/JoelDarioPerez.github.io/img/icasa.80b2b674.png"
+  },
+  {
+    "revision": "0055b24b4982648884e654b52cbbb46d",
+    "url": "/JoelDarioPerez.github.io/img/image.0055b24b.png"
+  },
+  {
+    "revision": "86765a610d570735b2d53591466c1a9f",
+    "url": "/JoelDarioPerez.github.io/img/katsuda.86765a61.png"
+  },
+  {
+    "revision": "a315a7e543123a9cb64ba475b2606a29",
+    "url": "/JoelDarioPerez.github.io/img/mapInLaptop.a315a7e5.png"
+  },
+  {
+    "revision": "d222ed2ac7ebcbb35c046ab3428983c2",
+    "url": "/JoelDarioPerez.github.io/img/mapInPhone.d222ed2a.png"
+  },
+  {
+    "revision": "45bbb8519c04d62046e2b4ad2e67c20c",
+    "url": "/JoelDarioPerez.github.io/img/mitre-salud.45bbb851.png"
+  },
+  {
+    "revision": "198eebdbd8222fd31bea3d3a5495a515",
+    "url": "/JoelDarioPerez.github.io/img/ochoveinte.198eebdb.png"
+  },
+  {
+    "revision": "1d1d7ba98145b01b6ce1f8b44150786d",
+    "url": "/JoelDarioPerez.github.io/img/taxis.1d1d7ba9.png"
+  },
+  {
+    "revision": "358fabb042395c9c279b4cc4d97d84e6",
+    "url": "/JoelDarioPerez.github.io/img/tractor.358fabb0.jpg"
+  },
+  {
+    "revision": "603c685e99c16b3124d0f448a1c60221",
+    "url": "/JoelDarioPerez.github.io/img/transporteDeCarga.603c685e.png"
+  },
+  {
+    "revision": "61215f15974f57e1b961a61d4f6836c7",
+    "url": "/JoelDarioPerez.github.io/img/urena.61215f15.png"
+  },
+  {
+    "revision": "79c0c79d51fdf079068b4a3f73e2f813",
+    "url": "/JoelDarioPerez.github.io/index.html"
+  },
+  {
+    "revision": "280b1ad754d41bd91df5",
+    "url": "/JoelDarioPerez.github.io/js/app.53f84fc2.js"
+  },
+  {
+    "revision": "a9c53ee77e1da5932aa3",
+    "url": "/JoelDarioPerez.github.io/js/chunk-vendors.c4a0ac7a.js"
+  },
+  {
+    "revision": "b90eb40bf7927022b66719a6a7bdd266",
+    "url": "/JoelDarioPerez.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/JoelDarioPerez.github.io/robots.txt"
+  }
+]);
